2841fe53d159bc2ce1928fdf1d920e482e1d84e0
pepe_fabra_valverde <jofaval@outlook.com>
2023-04-19 21:28:41 +0200

    feat(utilities/zod): add pollution use case

--- utilities/validator-inferred.utilities.ts
+++ utilities/validator-inferred.utilities.ts
@@ -1,5 +1,6 @@
 import z from "zod";
 
+// (not) surprisingly same as: https://stackoverflow.com/questions/70415330/do-not-allow-extra-properties-with-zod-parse
 const SuperImportantObject = z.object({
   foo: z.string(),
   bar: z.number(),
@@ -11,8 +12,23 @@ type SuperImportantObjectType = z.infer<typeof SuperImportantObject>;
 const throwsError = { foo: 1, bar: "a" } as SuperImportantObjectType;
 
 const parseResult = SuperImportantObject.safeParse({ foo: 1, bar: "a" });
-if (parseResult.success) {
-  const { bar, foo } = parseResult.data;
-} else {
+if (!parseResult.success) {
   // warn the user
+} else {
+  const { bar, foo } = parseResult.data;
+}
+
+const filterContaminationResult = SuperImportantObject.safeParse({
+  foo: "bar",
+  bar: 1,
+  pollution: true,
+});
+
+if (!filterContaminationResult.success) {
+  // warn the user
+} else {
+  const { bar, foo } = filterContaminationResult.data;
+  // note: weird that typescript doesn't filter the autocomplete to it's narrowest type
+  console.assert(typeof bar === "number");
+  console.assert(typeof foo === "string");
 }
